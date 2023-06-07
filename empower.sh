072bb52defbec572a3d44641b0ca9f6f233d4978
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-06-07 21:45:28 +0300

    Update empower.sh

--- empower.sh
+++ empower.sh
@@ -47,7 +47,7 @@ fi
 if [ ! $WALLET ]; then
 	echo "export WALLET=wallet" >> $HOME/.bash_profile
 fi
-echo "export EMPOWER_CHAIN_ID=altruistic-1" >> $HOME/.bash_profile
+echo "export EMPOWER_CHAIN_ID=circulus-1" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 # update
@@ -75,10 +75,9 @@ go version
 
 # download binary
 cd $HOME
-git clone https://github.com/empowerchain/empowerchain
-cd empowerchain
-git checkout v0.0.3
-cd chain
+git clone https://github.com/EmpowerPlastic/empowerchain
+cd empowerchain/chain
+git checkout v1.0.0-rc1
 make install
 
 # config
@@ -89,35 +88,39 @@ empowerd config keyring-backend test
 empowerd init $NODENAME --chain-id $EMPOWER_CHAIN_ID
 
 # download genesis and addrbook
-curl -s https://raw.githubusercontent.com/empowerchain/empowerchain/main/testnets/altruistic-1/genesis.json > $HOME/.empowerchain/config/genesis.json
+curl -s https://raw.githubusercontent.com/EmpowerPlastic/empowerchain/main/testnets/circulus-1/genesis.json > $HOME/.empowerchain/config/genesis.json
 curl -s https://snapshots2-testnet.nodejumper.io/empower-testnet/addrbook.json > $HOME/.empowerchain/config/addrbook.json
 
 # set minimum gas price
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025umpwr\"/;" $HOME/.empowerchain/config/app.toml
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.001umpwr\"/;" $HOME/.empowerchain/config/app.toml
 
 # set peers and seeds
-PEERS="ca8b9d5fecd3258cb8bb4164017114898cd63ad5@empower-testnet.nodejumper.io:31656,6dae9286b4ef23151148922befc0f32a00cc1ec4@65.21.134.202:26656,ab4b4331d161cf0e98d3244e30225e4f38ac8d2f@65.109.28.177:44656,d9307a7ba665a54e65f4fa5dbb5401448e1c3456@65.109.30.117:30656,46b552c62df0523a2bfff285eb384e4b197484aa@65.21.133.125:33656,408980a63332b230a90ad549e93162dab303836f@65.108.225.158:17456,605b175a3cf6f71d454840baef08d0e81d94935f@65.108.52.192:46656,86669cd5e5914f862578d43de483f49e93d396b1@51.83.35.129:26656,b405572f7bf70f681d1e82f196e1399bf90a9d8a@138.201.197.163:26656,c5d44acd2f0ee122352d2f8154d9b29aeb9bf0ec@159.69.65.97:36656,2b3da30140b57d64a57a25485c237f9c7c3c3324@194.163.136.90:26656,8abceaabc650d81a751e40382f80af6c98ba466f@185.239.209.180:35656,333de3fc2eba7eead24e0c5f53d665662b2ba001@35.187.86.119:26656,b5df76282e8704d253012688613d4eb725d3cb12@77.37.176.99:56656,8498049b61177a53b3f0e6b8f7c4a574251a2bbb@149.102.157.96:36656,56d05d4ae0e1440ad7c68e52cc841c424d59badd@96.234.160.22:26656"
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.empowerchain/config/config.toml
 SEEDS=""
-sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.empowerchain/config/config.toml
+PEERS="b897014f22e932e461e7fc98353a57d642dbe16e@empower-testnet.nodejumper.io:32656,0e56c10d30f4f16f5be4146bb905471ba99051df@86.48.17.208:15056,95ea7999e3ecd3fb7fd73fae70b3b29a6af24c8d@46.4.5.45:17456,1b4d9789c27befae430538d4364d012df61df896@65.109.82.112:2446,42e60e3b5784c1f670450376a095e221e4b0edc3@157.90.152.90:15056"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.empowerchain/config/config.toml
+
+# disable indexing
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.empowerchain/config/config.toml
 
 # config pruning
-pruning="custom" && \
-pruning_keep_recent="100" && \
-pruning_keep_every="0" && \
-pruning_interval="50" && \
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.empowerchain/config/app.toml && \
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.empowerchain/config/app.toml && \
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.empowerchain/config/app.toml && \
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.empowerchain/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.empowerchain/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.empowerchain/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.empowerchain/config/app.toml
+sed -i "s/snapshot-interval *=.*/snapshot-interval = 0/g" $HOME/.empowerchain/config/app.toml
 
-# reset
-empowerd unsafe-reset-all --home $HOME/.empowerchain
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.empowerchain/config/config.toml
 
 # create service
 sudo tee /etc/systemd/system/empowerd.service > /dev/null << EOF
 [Unit]
-Description=Empower Chain Node
+Description=Empower Node
 After=network-online.target
 [Service]
 User=$USER
@@ -129,15 +132,14 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
 
-rm -rf $HOME/.empowerchain/data 
-
-SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/empower-testnet/ | egrep -o ">altruistic-1.*\.tar.lz4" | tr -d ">")
-curl https://snapshots2-testnet.nodejumper.io/empower-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.empowerchain
+# reset
+empowerd tendermint unsafe-reset-all --home $HOME/.empowerchain --keep-addr-book 
+curl https://snapshots2-testnet.nodejumper.io/empower-testnet/circulus-1_2023-06-07.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.empowerchain
 
 # start service
-systemctl daemon-reload && \
-systemctl enable empowerd && \
-systemctl restart empowerd && journalctl -u empowerd -f -o cat
+sudo systemctl daemon-reload
+sudo systemctl enable empowerd
+sudo systemctl restart empowerd
 
 break
 ;;
@@ -157,16 +159,21 @@ break
 ;;
 
 "Create Validator")
+
 empowerd tx staking create-validator \
-  --amount 1000000umpwr \
-  --from $WALLET \
-  --commission-max-change-rate 0.1 \
-  --commission-max-rate 0.2 \
-  --commission-rate 0.05 \
-  --min-self-delegation "1000000" \
-  --pubkey  $(empowerd tendermint show-validator) \
-  --moniker $NODENAME \
-  --chain-id $EMPOWER_CHAIN_ID
+--amount=1000000umpwr \
+--pubkey=$(empowerd tendermint show-validator) \
+--moniker $NODENAME \
+--chain-id $EMPOWER_CHAIN_ID \
+--commission-rate=0.10 \
+--commission-max-rate=0.20 \
+--commission-max-change-rate=0.01 \
+--min-self-delegation=1 \
+--from $WALLET \
+--gas-prices=0.1umpwr \
+--gas-adjustment=1.5 \
+--gas=auto \
+-y
   
 break
 ;;
